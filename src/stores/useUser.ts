9a8aba4e0488055e22031750f12593c5180bc900
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-19 19:25:42 -0300

    login and logout

--- src/stores/useUser.ts
+++ src/stores/useUser.ts
@@ -3,6 +3,8 @@ import { IUser } from "../interfaces/user.interface";
 import { IRegisterUserData } from "../data/user/register";
 import { Dispatch, SetStateAction } from "react";
 import { userRequest } from "../data/user/_index";
+import { ILoginUserData } from "../data/user/login";
+import { LocalStorageKeys } from "../enum/LocalStorageKeys.enum";
 
 interface Store {
   user: IUser | null;
@@ -12,6 +14,12 @@ interface Store {
     setLoading: Dispatch<SetStateAction<boolean>>,
     callback?: () => void
   ) => Promise<void>;
+  login: (
+    formData: ILoginUserData,
+    setLoading: Dispatch<SetStateAction<boolean>>,
+    callback?: () => void
+  ) => Promise<void>;
+  logout: () => void;
 }
 
 export const useUser = create<Store>((set) => ({
@@ -19,14 +27,31 @@ export const useUser = create<Store>((set) => ({
   loading: false,
   register: async (formData, setLoading, callback) => {
     try {
-        setLoading(true);
-        await userRequest.register(formData);
-        console.log("Usuário cadastro com sucesso!");
-        if(callback) callback();
+      setLoading(true);
+      await userRequest.register(formData);
+      console.log("Usuário cadastro com sucesso!");
+      if (callback) callback();
     } catch (error) {
-        console.log(error);
+      console.log(error);
     } finally {
-        setLoading(false);
+      setLoading(false);
     }
+  },
+  login: async (formData, setLoading, callback) => {
+    try {
+      setLoading(true);
+      const data = await userRequest.login(formData);
+      set({ user: data.user });
+      localStorage.setItem(LocalStorageKeys.TOKEN, data.accessToken);
+      if (callback) callback();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  },
+  logout: () => {
+    set({ user: null });
+    localStorage.removeItem(LocalStorageKeys.TOKEN);
   },
 }));
