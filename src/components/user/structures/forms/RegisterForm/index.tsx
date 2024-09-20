59d51da1712129b2199c52aa6c78d5c7700494b7
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-20 09:28:52 -0300

    register form part 1

---
+++ src/components/user/structures/forms/RegisterForm/index.tsx
@@ -0,0 +1,11 @@
+import { useForm } from "react-hook-form"
+
+export function RegisterForm(){
+    const { register, handleSubmit, formState: { errors }} = useForm();
+
+    return(
+        <form>
+
+        </form>
+    )
+}
