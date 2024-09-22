65b41d8f403074eac111584829bc2f4139c1e01e
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-22 11:48:14 -0300

    user advert store get many action

--- src/stores/userAdvert/useUserAdvert.ts
+++ src/stores/userAdvert/useUserAdvert.ts
@@ -7,26 +7,44 @@ import { LocalStorageKeys } from "../../enum/LocalStorageKeys.enum";
 
 interface Store {
   advertList: IAdvert[];
+  loading: boolean;
   createAdvert: (
     formData: TAdvertCreateData,
     setLoading: Dispatch<SetStateAction<boolean>>,
     callback?: () => void
   ) => Promise<void>;
+  getAdvertList: (callback?: () => void) => Promise<void>;
 }
 
 export const useUserAdvert = create<Store>((set) => ({
   advertList: [],
+  loading: false,
+
   createAdvert: async (formData, setLoading, callback) => {
     try {
       setLoading(true);
       const token = localStorage.getItem(LocalStorageKeys.TOKEN);
       const data = await advertRequest.create(formData, token as string);
       set(({ advertList }) => ({ advertList: [...advertList, data] }));
-      if(callback) callback();
+      if (callback) callback();
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
     }
   },
+  
+  getAdvertList: async (callback) => {
+    try {
+        set({ loading: true });
+        const token = localStorage.getItem(LocalStorageKeys.TOKEN);
+        const data = await advertRequest.getManyFromUser(token as string);
+        set({ advertList: data });
+        if(callback) callback();
+    } catch (error) {
+        console.log(error);
+    } finally {
+        set({ loading: false });
+    }
+  }
 }));
