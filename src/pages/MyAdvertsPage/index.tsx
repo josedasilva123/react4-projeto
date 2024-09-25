c3c087ac2ebb39a9cd93e2f91f598acd4b3d79d1
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-25 08:59:27 -0300

    user advert page

--- src/pages/MyAdvertsPage/index.tsx
+++ src/pages/MyAdvertsPage/index.tsx
@@ -1,7 +1,24 @@
-export function MyAdvertsPage(){
-    return(
-        <>
-            <h1>Meus anúncios</h1>
-        </>
-    )
+import { UserAdvertListLoader } from "../../components/userAdvert/loaders/UserAdvertListLoader";
+import { UserAdvertList } from "../../components/userAdvert/structures/lists/UserAdvertList";
+import { CreateAdvertModal } from "../../components/userAdvert/structures/modals/CreateAdvertModal";
+import { EditAdvertModal } from "../../components/userAdvert/structures/modals/EditAdvertModal";
+import { RemoveAdvertModal } from "../../components/userAdvert/structures/modals/RemoveAdvertModal";
+import { useUserAdvert } from "../../stores/userAdvert/useUserAdvert";
+
+export function MyAdvertsPage() {
+  const loading = useUserAdvert((store) => store.loading);
+
+  return (
+    <UserAdvertListLoader>
+      <section>
+        <div>
+          <h1>Meus anúncios</h1>
+          <CreateAdvertModal />
+        </div>
+        <div>{loading ? <UserAdvertList /> : <p>Carregando...</p>}</div>
+      </section>
+      <EditAdvertModal />
+      <RemoveAdvertModal />
+    </UserAdvertListLoader>
+  );
 }
