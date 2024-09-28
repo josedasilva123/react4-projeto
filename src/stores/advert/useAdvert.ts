ff96a7091f5562d52187491119d2b5c81bb9846d
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-28 10:37:12 -0300

    pagination action

--- src/stores/advert/useAdvert.ts
+++ src/stores/advert/useAdvert.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 import { IAdvert } from "../../interfaces/advert.interface";
 import { requestAction } from "../../utils/requestAction";
-import { advertApi } from "../../api/advert.api";
 import { advertRequest } from "../../data/advert/_index";
+import { Dispatch, SetStateAction } from "react";
 
 interface Store {
   loading: boolean;
@@ -12,30 +12,61 @@ interface Store {
   search: "";
   skip: number;
   getManyAdverts: (search: string, order: "asc" | "desc") => Promise<void>;
+  getNextAdvertPage: (
+    search: string,
+    order: "asc" | "desc",
+    setLoading: Dispatch<SetStateAction<boolean>>
+  ) => Promise<void>;
 }
 
-export const useAdvert = create<Store>((set) => ({
+const PAGE_COUNT = 5;
+
+export const useAdvert = create<Store>((set, get) => ({
   loading: false,
   advertList: [],
   total: 0,
   order: "desc",
   search: "",
   skip: 0,
-  
+
   getManyAdverts: async (search, order) => {
     requestAction({
       action: async () => {
         const data = await advertRequest.getMany({
-          count: 5,
+          count: PAGE_COUNT,
           order,
           search,
           skip: 0,
         });
 
-        set({ advertList: data.adverts, total: data.count });
+        set({ skip: 0, advertList: data.adverts, total: data.count });
       },
       onInit: () => set({ loading: true }),
-      onEnd: () => set({ loading: false })
+      onEnd: () => set({ loading: false }),
+    });
+  },
+
+  getNextAdvertPage: async (search, order, setLoading) => {
+    requestAction({
+      action: async () => {
+        const { skip } = get();
+
+        const newSkip = skip + PAGE_COUNT;
+
+        const data = await advertRequest.getMany({
+          count: PAGE_COUNT,
+          order,
+          search,
+          skip: newSkip,
+        });
+
+        set(({ advertList }) => ({
+          skip: newSkip,
+          advertList: [...advertList, ...data.adverts],
+          total: data.count,
+        }));
+      },
+      setLoading,
     });
   },
 }));
