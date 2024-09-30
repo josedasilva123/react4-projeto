e6ea5f76be1069e0c39a511343bc119cd4703f5c
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-09-30 17:43:45 -0300

    advert pagination part 2

--- src/components/advert/structures/lists/AdvertList/AdvertPagination/index.tsx
+++ src/components/advert/structures/lists/AdvertList/AdvertPagination/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAdvert } from "../../../../../../stores/advert/useAdvert";
+import { PAGE_COUNT, useAdvert } from "../../../../../../stores/advert/useAdvert";
 import { Button } from "../../../../../shared/fragments/buttons/Button";
 
 export function AdvertPagination() {
@@ -9,17 +9,23 @@ export function AdvertPagination() {
 
   const order = useAdvert((store) => store.order);
 
+  const skip = useAdvert((store) => store.skip);
+
+  const total = useAdvert((store) => store.total);
+
   const getNextAdvertPage = useAdvert((store) => store.getNextAdvertPage);
+
+  const isVisible = total > skip + PAGE_COUNT;
 
   function handleClick() {
     getNextAdvertPage(search, order, setLoading);
   }
 
-  return (
+  return isVisible ? (
     <div>
       <Button onClick={handleClick} disabled={loading}>
         {loading ? "Carregando..." : "Carregar mais"}
       </Button>
     </div>
-  );
+  ) : null;
 }
