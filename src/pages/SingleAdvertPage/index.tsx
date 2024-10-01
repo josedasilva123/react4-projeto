813efaca38585a8d7050d33cd187728d900a6fd8
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-01 08:07:26 -0300

    single advert page part 1

--- src/pages/SingleAdvertPage/index.tsx
+++ src/pages/SingleAdvertPage/index.tsx
@@ -1,7 +1,30 @@
-export function SingleAdvertPage(){
-    return(
-        <>
-            <h1>Anúncio (interna)</h1>
-        </>
-    )
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IAdvert } from "../../interfaces/advert.interface";
+import { requestAction } from "../../utils/requestAction";
+import { advertRequest } from "../../data/advert/_index";
+
+export function SingleAdvertPage() {
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [advert, setAdvert] = useState<IAdvert | null>(null);
+
+  useEffect(() => {
+    requestAction({
+      action: async () => {
+        const data = await advertRequest.getOne(id as string);
+        setAdvert(data);
+      },
+      setLoading,
+    });
+  }, [id]);
+
+  console.log(advert);
+
+  return (
+    <>
+      <h1>Anúncio (interna)</h1>
+    </>
+  );
 }
