2afb1e8e360d78df86de2cab384b08a08ba916ff
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-03 11:13:05 -0300

    reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { DefaultTemplate } from "./components/shared/template/DefaultTemplate";
 import { ProfileLoader } from "./components/user/loaders/ProfileLoader";
 import { Router } from "./routes/Router";
+import "./styles/index.scss"
 
 function App() {
   return (
