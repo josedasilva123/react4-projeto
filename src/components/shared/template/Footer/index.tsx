0b5367a703bee43d644e66b1c19708838f6ca0e9
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-08 18:18:41 -0300

    footer styles

--- src/components/shared/template/Footer/index.tsx
+++ src/components/shared/template/Footer/index.tsx
@@ -1,16 +1,19 @@
 import Logo from "../../../../assets/LXLogo.svg";
 import { Container } from "../../fragments/containers/Container";
 import { Text } from "../../fragments/typography/Text";
+import styles from "./style.module.scss";
 
 export function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <Container size="big">
-        <img src={Logo} alt="Logo LX Anúncios" />
+        <div className={styles.box}>
+          <img src={Logo} alt="Logo LX Anúncios" />
 
-        <Text className="text small black50" tag="p">
-          Todos os direitos reservados
-        </Text>
+          <Text className="text small black50" tag="p">
+            Todos os direitos reservados
+          </Text>
+        </div>
       </Container>
     </footer>
   );
