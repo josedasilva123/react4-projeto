a233355867f5ca9896f0c8660636f55a16a6979a
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-08 13:45:32 -0300

    logout header styles

--- src/components/shared/template/Header/UserControls/index.tsx
+++ src/components/shared/template/Header/UserControls/index.tsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import { useUser } from "../../../../../stores/user/useUser";
 import { Text } from "../../../fragments/typography/Text";
 import { Icon } from "../../../fragments/icons/Icon";
+import styles from "./style.module.scss";
 
 export function UserControls() {
   const user = useUser((store) => store.user);
   const logout = useUser((store) => store.logout);
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <nav>
         <ul>
           <li>
@@ -20,16 +21,18 @@ export function UserControls() {
           </li>
         </ul>
       </nav>
-      <div>
+      <div className={styles.userBox}>
         <span>{user?.name.charAt(0)}</span>
-        <Text className="text small" tag="p">
-          {user?.name}
-        </Text>
-        <Text className="text small" tag="p">
-          {user?.email}
-        </Text>
+        <div>
+          <Text className="text small" tag="p">
+            {user?.name}
+          </Text>
+          <Text className="text small" tag="p">
+            {user?.email}
+          </Text>
+        </div>
       </div>
-      <div>
+      <div className={styles.buttonBox}>
         <Link to="/meu-perfil" title="Configurações" aria-label="Configurações">
           <Icon color="blue" icon="settings" />
         </Link>
