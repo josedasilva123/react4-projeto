803dddaa23f79605e9f564dfac7ac728001620c6
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-08 18:33:49 -0300

    home styles part1

--- src/components/advert/structures/lists/AdvertList/AdvertCard/index.tsx
+++ src/components/advert/structures/lists/AdvertList/AdvertCard/index.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { IAdvert } from "../../../../../../interfaces/advert.interface";
 import { Text } from "../../../../../shared/fragments/typography/Text";
 import { Price } from "../../../../../shared/fragments/typography/Price";
+import styles from "./style.module.scss";
+import { toMoney } from "../../../../../../utils/toMoney";
 
 interface Props {
   advert: IAdvert;
@@ -9,20 +11,24 @@ interface Props {
 
 export function AdvertCard({ advert }: Props) {
   return (
-    <li>
+    <li className={styles.card}>
       <Link to={`/anuncio/${advert.id}`}>
         {advert.image ? (
           <img src={advert.image} alt={`Ilustração ${advert.title}`} />
         ) : null}
 
-        <div>
-          <Text className="text" tag="h2">{advert.title}</Text>
-          <Text className="text small black50" tag="p">
-            {advert.excerpt}
-          </Text>
-        </div>
+        <div className={styles.content}>
+          <div>
+            <Text className="text" tag="h2">
+              {advert.title}
+            </Text>
+            <Text className="text small black50" tag="p">
+              {advert.excerpt}
+            </Text>
+          </div>
 
-        <Price tag="p">{advert.price}</Price>
+          <Price tag="p">{toMoney(advert.price)}</Price>
+        </div>
       </Link>
     </li>
   );
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,16 @@
 import { AdvertListLoader } from "../../components/advert/loaders/AdvertListLoader";
 import { AdvertList } from "../../components/advert/structures/lists/AdvertList";
 import { Container } from "../../components/shared/fragments/containers/Container";
+import styles from "./style.module.scss";
 
 export function HomePage() {
   return (
     <AdvertListLoader>
-      <Container>
-        <AdvertList />
-      </Container>
+      <section className={styles.section}>
+        <Container>
+          <AdvertList />
+        </Container>
+      </section>
     </AdvertListLoader>
   );
 }
