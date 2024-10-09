e1d05c89d6d60815067e7c1a8cca2c0ac131a66e
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-09 08:48:45 -0300

    login styles

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -3,20 +3,25 @@ import { Container } from "../../components/shared/fragments/containers/Containe
 import { Text } from "../../components/shared/fragments/typography/Text";
 import { Title } from "../../components/shared/fragments/typography/Title";
 import { LoginForm } from "../../components/user/structures/forms/LoginForm";
+import styles from "./style.module.scss";
 
 export function LoginPage() {
   return (
-    <Container size="extra-small">
-      <Title className="title three" tag="h1">
-        Login
-      </Title>
-      <LoginForm />
-      <Text className="text small center" tag="p">
-        ou
-      </Text>
-      <ButtonLink className="button big blue-outline full" to={"/cadastro"}>
-        Cadastre-se
-      </ButtonLink>
-    </Container>
+    <section className={styles.section}>
+      <Container size="extra-small">
+        <div className={`border-box ${styles.box}`}>
+          <Title className="title three" tag="h1">
+            Login
+          </Title>
+          <LoginForm />
+          <Text className="text small center" tag="p">
+            ou
+          </Text>
+          <ButtonLink className="button big blue-outline full" to={"/cadastro"}>
+            Cadastre-se
+          </ButtonLink>
+        </div>
+      </Container>
+    </section>
   );
 }
