88e21ebfb6a6df31a6b8c8e098925fd9c71ac15d
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-09 18:34:47 -0300

    global modal styles

--- src/components/shared/structures/Modal/index.tsx
+++ src/components/shared/structures/Modal/index.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom";
 import { useOutClick } from "../../../../hooks/useOutClick";
 import { Icon } from "../../fragments/icons/Icon";
+import styles from "./style.module.scss";
 
 interface Props {
   children: React.ReactNode;
@@ -11,9 +12,10 @@ export function Modal({ children, onClose }: Props) {
   const modalRef = useOutClick<HTMLDivElement>(onClose);
 
   return createPortal(
-    <div>
-      <div ref={modalRef} role="dialog">
+    <div className={styles.overlay}>
+      <div className={styles.box} ref={modalRef} role="dialog">
         <button
+          className={styles.close}
           onClick={onClose}
           title="Fechar modal"
           aria-label="Fechar modal"
