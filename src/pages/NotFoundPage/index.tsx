e42c0b0b85566fb6963f388effeb8c2dbf906f64
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-10-10 09:13:12 -0300

    not found page styles

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -2,19 +2,25 @@ import { ButtonLink } from "../../components/shared/fragments/buttons/ButtonLink
 import { Container } from "../../components/shared/fragments/containers/Container";
 import { Text } from "../../components/shared/fragments/typography/Text";
 import { Title } from "../../components/shared/fragments/typography/Title";
+import { FullHeightSection } from "../../components/shared/structures/FullHeightSection";
+import styles from "./style.module.scss";
 
 export function NotFoundPage() {
   return (
-    <Container size="small">
-      <Title className="title one center" tag="h1">
-        Não encontrado - 404
-      </Title>
-      <Text className="text center" tag="p">
-        Clique no botão a baixo para voltar para página inicial
-      </Text>
-      <ButtonLink className="button small blue-outline" to={"/"}>
-        Voltar para home
-      </ButtonLink>
-    </Container>
+    <FullHeightSection tag="section" backgroundColor="gray">
+      <Container size="small">
+        <div className={styles.box}>
+          <Title className="title one center" tag="h1">
+            Não encontrado - 404
+          </Title>
+          <Text className="text center" tag="p">
+            Clique no botão a baixo para voltar para página inicial
+          </Text>
+          <ButtonLink className="button big blue-outline" to={"/"}>
+            Voltar para home
+          </ButtonLink>
+        </div>
+      </Container>
+    </FullHeightSection>
   );
 }
